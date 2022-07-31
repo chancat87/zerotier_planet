d1f3e7129436a66ece107cf92dd7ecb04bb3e04b
chancat <ql2445@126.com>
2022-07-31 22:58:06 +0800

    Update zertotier_planet_debain.sh
    修改ztncui_0.8.7

--- zertotier_planet_debain.sh
+++ zertotier_planet_debain.sh
@@ -27,8 +27,8 @@ mv ./world.bin ./planet
 \cp -r ./planet /var/lib/zerotier-one/
 \cp -r ./planet /root
 systemctl restart zerotier-one.service
-wget https://gitee.com/opopop880/ztncui/attach_files/932632/download/ztncui_0.8.6_amd64.deb
-sudo dpkg -i ztncui_0.8.6_amd64.deb
+wget https://github.com/ly88321/ztncui-zh/releases/download/ztncui_0.8.7/ztncui_0.8.7_amd64.deb
+sudo dpkg -i ztncui_0.8.7_amd64.deb
 cd /opt/key-networks/ztncui/
 echo "HTTPS_PORT = 3443" >>./.env
 secret=`cat /var/lib/zerotier-one/authtoken.secret`
